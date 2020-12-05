33c77c71ff5d984b520698c10f7e50792af16ed1
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-12-05 12:56:09 +0100

    db demo second part

--- src/main/java/pl/sda/spring/demo/dbdemo/CarConfiguration.java
+++ src/main/java/pl/sda/spring/demo/dbdemo/CarConfiguration.java
@@ -1,5 +1,6 @@
 package pl.sda.spring.demo.dbdemo;
 
+import lombok.val;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -15,6 +16,15 @@ public class CarConfiguration {
 
     @PostConstruct
     public void saveCarsToRepository(){
+        CarEntity ford = new CarEntity();
+        ford.setMake("Ford");
+        ford.setModel("Mustang");
 
+        CarEntity fiat = new CarEntity();
+        fiat.setMake("Fiat");
+        fiat.setModel("125P");
+
+        carRepository.save(ford);
+        carRepository.save(fiat);
     }
 }
