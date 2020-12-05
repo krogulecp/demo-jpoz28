d09326568d3fe2e6ac1d6d5b4c79394d3cfa098d
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-12-05 14:58:26 +0100

    task06 p2

--- src/main/java/pl/sda/spring/demo/ex6/AnimalRepository.java
+++ src/main/java/pl/sda/spring/demo/ex6/AnimalRepository.java
@@ -2,5 +2,9 @@ package pl.sda.spring.demo.ex6;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnimalRepository extends JpaRepository<AnimalEntity, Long> {
+
+    List<AnimalEntity> findByAgeGreaterThanAndNameContains(int age, String namePart);
 }
--- src/main/java/pl/sda/spring/demo/ex6/Task06Controller.java
+++ src/main/java/pl/sda/spring/demo/ex6/Task06Controller.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.spring.demo.util.Animal;
 
@@ -33,5 +34,15 @@ public class Task06Controller {
 
         return ResponseEntity.ok()
                 .body(animals);
+    }
+
+    @GetMapping("/task06.2")
+    public ResponseEntity<List<Animal>> getAnimals(@RequestParam("ageGreaterThan") int ageGreaterThan,
+                                                   @RequestParam("nameContains") String nameContains){
+        final List<Animal> animals = animalRepository.findByAgeGreaterThanAndNameContains(ageGreaterThan, nameContains).stream()
+                .map(animalEntity -> new Animal(animalEntity.getName(), animalEntity.getAge()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(animals);
     }
 }
