c49db3ae309a8097afd6ea79a1970d062c46f1a5
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-12-06 11:00:46 +0100

    jpa exercise p2

--- src/test/java/pl/sda/spring/demo/dbdemo/CarRepositoryTest.java
+++ src/test/java/pl/sda/spring/demo/dbdemo/CarRepositoryTest.java
@@ -1,9 +1,11 @@
 package pl.sda.spring.demo.dbdemo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,12 +16,17 @@ class CarRepositoryTest {
     @Autowired
     private CarRepository carRepository;
 
+    @BeforeEach
+    void setUpCars(){
+        final CarEntity fiat126 = new CarEntity(null, "Fiat", "126P");
+        final CarEntity fiat500 = new CarEntity(null, "Fiat", "500");
+        final CarEntity ford = new CarEntity(null, "Ford", "Escort");
+
+        carRepository.saveAll(List.of(fiat126, fiat500, ford));
+    }
+
     @Test
     void should_find_car(){
-        //given
-        final CarEntity fiat = new CarEntity(null, "Fiat", "126P");
-        carRepository.save(fiat);
-
         //when
         final List<CarEntity> result = carRepository.findByModel("126P");
 
@@ -29,4 +36,18 @@ class CarRepositoryTest {
         assertThat(carEntity.getMake()).isEqualTo("Fiat");
         assertThat(carEntity.getModel()).isEqualTo("126P");
     }
+
+    @Test
+    void should_find_car_by_make_and_model(){
+        //when
+        final List<CarEntity> result = carRepository.findByMakeAndModel("Ford", "Escort");
+
+        //then
+        assertThat(result.size()).isEqualTo(1);
+        final CarEntity fordResult = result.get(0);
+        assertThat(fordResult.getMake()).isEqualTo("Ford");
+        assertThat(fordResult.getModel()).isEqualTo("Escort");
+    }
+
+    //napiszmy test, który zweryfikuje wyszukanie wszystkich samochodów
 }
