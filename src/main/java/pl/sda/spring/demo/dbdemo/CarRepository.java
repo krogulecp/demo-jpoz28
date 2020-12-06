81ec5784bfe371769c7a2feebcc5c72bc3740d76
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-12-06 09:17:32 +0100

    custom repository impl

--- src/main/java/pl/sda/spring/demo/dbdemo/CarRepository.java
+++ src/main/java/pl/sda/spring/demo/dbdemo/CarRepository.java
@@ -1,6 +1,20 @@
 package pl.sda.spring.demo.dbdemo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-interface CarRepository extends JpaRepository<CarEntity,Long> {
+interface CarRepositoryCustom {
+    void doSomeCustomStuff();
+}
+
+interface CarRepository extends JpaRepository<CarEntity,Long>, CarRepositoryCustom {
+}
+
+@Repository
+class CarRepositoryCustomImpl implements CarRepositoryCustom {
+
+    @Override
+    public void doSomeCustomStuff() {
+
+    }
 }
