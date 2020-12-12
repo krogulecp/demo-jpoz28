861cb7755be796e7d81481893125845558a66e3d
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-12-12 15:41:01 +0100

    swagger

--- src/main/java/pl/sda/spring/demo/ex8/AnimalValidationController.java
+++ src/main/java/pl/sda/spring/demo/ex8/AnimalValidationController.java
@@ -1,11 +1,13 @@
 package pl.sda.spring.demo.ex8;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Api
 @RestController
 public class AnimalValidationController {
 
